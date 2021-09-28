62589c603f5119713fb5db1edd9c4a38bf0b5e5b
AshenAwakener <80026265+AshenAwakener@users.noreply.github.com>
2021-09-28 19:51:13 +0800

    More modifications

--- app/src/main/java/com/example/biskwit/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/biskwit/ui/home/HomeFragment.java
@@ -1,39 +1,47 @@
 package com.example.biskwit.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import android.widget.Button;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-
+import com.example.biskwit.Lesson;
 import com.example.biskwit.R;
-import com.example.biskwit.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-    private FragmentHomeBinding binding;
+    Button LessonButton;
+    Intent intent;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
+    @Override
+    public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        return root;
+        View rootView = inflater.inflate(R.layout.fragment_home,
+                container, false);
+        LessonButton = (Button) rootView.findViewById(R.id.LessonButton);
+        LessonButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoToLesson();
+            }
+        });
+        return rootView;
+    }
+    public void GoToLesson()
+    {
+        intent = new Intent(getActivity(), Lesson.class);
+        startActivity(intent);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
     }
 }
