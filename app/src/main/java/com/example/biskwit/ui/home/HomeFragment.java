c6d930808d017c4b1db2c246e509109990137460
AshenAwakener <80026265+AshenAwakener@users.noreply.github.com>
2021-09-29 14:49:36 +0800

    New features

--- app/src/main/java/com/example/biskwit/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/biskwit/ui/home/HomeFragment.java
@@ -7,41 +7,62 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
-import com.example.biskwit.Lesson;
+import com.example.biskwit.LessonFragment;
 import com.example.biskwit.R;
+import com.example.biskwit.databinding.FragmentHomeBinding;
+import com.example.biskwit.storyFragment;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-    Button LessonButton;
+    FragmentHomeBinding binding;
     Intent intent;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
 
-        View rootView = inflater.inflate(R.layout.fragment_home,
-                container, false);
-        LessonButton = (Button) rootView.findViewById(R.id.LessonButton);
-        LessonButton.setOnClickListener(new View.OnClickListener() {
+        binding = FragmentHomeBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.LessonButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                GoToLesson();
+                Fragment fragmentLesson = new LessonFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.nav_host_fragment_content_main_nav_menu,fragmentLesson);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
             }
         });
-        return rootView;
-    }
-    public void GoToLesson()
-    {
-        intent = new Intent(getActivity(), Lesson.class);
-        startActivity(intent);
+        binding.StoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragmentStory = new storyFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.nav_host_fragment_content_main_nav_menu,fragmentStory);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        binding = null;
     }
 }
