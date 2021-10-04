61344bd472f74e1f79f5fcf70563ec6e9323311d
AshenAwakener <80026265+AshenAwakener@users.noreply.github.com>
2021-10-04 14:18:13 +0800

    Voice function added

--- app/src/main/java/com/example/biskwit/Content/Lessons/PatinigActivities/PatinigLesson1.java
+++ app/src/main/java/com/example/biskwit/Content/Lessons/PatinigActivities/PatinigLesson1.java
@@ -1,18 +1,52 @@
 package com.example.biskwit.Content.Lessons.PatinigActivities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.Locale;
 import com.example.biskwit.R;
 
 public class PatinigLesson1 extends AppCompatActivity {
+
+    TextView txtresult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patinig_lesson1);
 
+        txtresult = (TextView) findViewById(R.id.Word);
+    }
 
+    public void getSpeechInput(View view) {
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(this, "Your Device Don't Support Speech Input", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 10:
+                if (resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtresult.setText(result.get(0));
+                }
+                break;
+        }
     }
 }
