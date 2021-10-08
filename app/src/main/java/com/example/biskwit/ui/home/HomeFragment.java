8a81ae2d32f38bf2bee2e9fb558740cb91464785
AshenAwakener <80026265+AshenAwakener@users.noreply.github.com>
2021-10-09 01:07:43 +0800

    i'm pushing, intro video added

--- app/src/main/java/com/example/biskwit/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/biskwit/ui/home/HomeFragment.java
@@ -1,11 +1,16 @@
 package com.example.biskwit.ui.home;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
 import androidx.fragment.app.Fragment;
 
+import com.example.biskwit.R;
 import com.example.biskwit.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -17,18 +22,22 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
 
         // dito iniinitialize yung layout ng home and navbar
-        binding = FragmentHomeBinding.inflate(getLayoutInflater());
 
         container.removeAllViews();
 
-        return binding.getRoot();
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        MediaController mc= new MediaController(getActivity());
+
+        VideoView view = (VideoView)rootView.findViewById(R.id.Intro);
+        String path = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.intro;
+        view.setVideoURI(Uri.parse(path));
+        view.setMediaController(mc);
+        view.start();
+
+        return rootView;
     }
 
-    public void onViewCreated(View view, Bundle savedInstanceState)
-    {
-        super.onViewCreated(view, savedInstanceState);
 
-    }
 
     // need ito somehow para di magkabuhol buhol yung navigation thingy niya
     @Override
