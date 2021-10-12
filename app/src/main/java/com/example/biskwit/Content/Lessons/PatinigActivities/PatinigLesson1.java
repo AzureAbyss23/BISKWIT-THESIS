0137e584863914ef4e4d79bbd2f59a3832580d33
AshenAwakener <80026265+AshenAwakener@users.noreply.github.com>
2021-10-12 14:52:57 +0800

    ALGORITHM FINALLY IMPLEMENTED

--- app/src/main/java/com/example/biskwit/Content/Lessons/PatinigActivities/PatinigLesson1.java
+++ app/src/main/java/com/example/biskwit/Content/Lessons/PatinigActivities/PatinigLesson1.java
@@ -41,7 +41,7 @@ public class PatinigLesson1 extends AppCompatActivity {
     String word = "";
     DBHelper DB;
     Cursor c;
-    String[] P_Lesson_Words = {"aso","ahas","aklat","ama","anak","atis","alay","aliw","amihan"};
+    String[] P_Lesson_Words = {"aso","si juan tamad","ang aso ay naipit sa pintuan","ama","anak","atis","alay","aliw","amihan"};
     StringBuffer buff;
     int all_ctr = 0;
     int click = 0;
@@ -139,7 +139,8 @@ public class PatinigLesson1 extends AppCompatActivity {
             public void onResults(Bundle bundle) {
                 //micButton.setImageResource(R.drawable.ic_mic_black_off);
                 ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-                txtresult.setText(data.get(0));
+                word = data.get(0);
+                printSimilarity(word.toString(),P_Lesson_Words[all_ctr]);
             }
 
             @Override
@@ -195,6 +196,56 @@ public class PatinigLesson1 extends AppCompatActivity {
             if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 Toast.makeText(this,"Permission Granted",Toast.LENGTH_SHORT).show();
         }
+    }
+
+
+    // TRY NEW ALGORITHM
+    public static double similarity(String s1, String s2) {
+        String longer = s1, shorter = s2;
+        if (s1.length() < s2.length()) { // longer should always have greater length
+            longer = s2; shorter = s1;
+        }
+        int longerLength = longer.length();
+        if (longerLength == 0) { return 1.0; /* both strings are zero length */ }
+    /* // If you have Apache Commons Text, you can use it to calculate the edit distance:
+    LevenshteinDistance levenshteinDistance = new LevenshteinDistance();
+    return (longerLength - levenshteinDistance.apply(longer, shorter)) / (double) longerLength; */
+        return (longerLength - editDistance(longer, shorter)) / (double) longerLength;
+
+    }
+
+    // Example implementation of the Levenshtein Edit Distance
+    // See http://rosettacode.org/wiki/Levenshtein_distance#Java
+    public static int editDistance(String s1, String s2) {
+        s1 = s1.toLowerCase();
+        s2 = s2.toLowerCase();
+
+        int[] costs = new int[s2.length() + 1];
+        for (int i = 0; i <= s1.length(); i++) {
+            int lastValue = i;
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0)
+                    costs[j] = j;
+                else {
+                    if (j > 0) {
+                        int newValue = costs[j - 1];
+                        if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                            newValue = Math.min(Math.min(newValue, lastValue),
+                                    costs[j]) + 1;
+                        costs[j - 1] = lastValue;
+                        lastValue = newValue;
+                    }
+                }
+            }
+            if (i > 0)
+                costs[s2.length()] = lastValue;
+        }
+        return costs[s2.length()];
+    }
+
+    public void printSimilarity(String s, String t) {
+        txtresult.setText(String.format(
+                "%.3f", similarity(s, t), s, t));
     }
 }
 
