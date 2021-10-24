ff6a52309f7c22870364a024c18a0e2d674eb1e2
AshenAwakener <80026265+AshenAwakener@users.noreply.github.com>
2021-10-25 01:29:49 +0800

    DATABASE PALIT WITH ADJUST

--- app/src/main/java/com/example/biskwit/Content/Lessons/AlphabetActivities/Alphabet.java
+++ app/src/main/java/com/example/biskwit/Content/Lessons/AlphabetActivities/Alphabet.java
@@ -6,7 +6,11 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.database.Cursor;
@@ -22,11 +26,30 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.biskwit.Content.Lessons.Score;
 import com.example.biskwit.DBHelper;
+import com.example.biskwit.Data.Constants;
+
+import com.example.biskwit.MainActivity;
+import com.example.biskwit.MainNavMenu;
 import com.example.biskwit.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import retrofit2.Call;
+import retrofit2.Callback;
 
 public class Alphabet extends AppCompatActivity {
 
@@ -36,9 +59,7 @@ public class Alphabet extends AppCompatActivity {
     String word = "";
     DBHelper DB;
     Cursor c;
-    String[] P_Lesson_Words = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
-    String[] UpperCase = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-    StringBuffer buff;
+    String[] alphabet;
     int all_ctr = 0;
     int click = 0;
     int mic_ctr = 0;
@@ -50,6 +71,8 @@ public class Alphabet extends AppCompatActivity {
 
     private int CurrentProgress = 0;
     private ProgressBar progressBar;
+
+    ProgressDialog progressDialog;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -72,41 +95,28 @@ public class Alphabet extends AppCompatActivity {
         ai = MediaPlayer.create(Alphabet.this, R.raw.kab1);
         ai.start();
 
-        //DB = new DBHelper(this);
+        progressDialog = new ProgressDialog(Alphabet.this);
 
-        //c = DB.getlessondata(letter);
-
-        /*if(c.getCount()==0){
-            Toast.makeText(this, "No data...", Toast.LENGTH_SHORT).show();
-            return;
-        } else {
-            for (int i = 0;c.moveToNext();i++) {
-                buff = new StringBuffer();
-                //buff.append(c.getString(c.getColumnIndex("P_Lesson_Word")));
-                //P_Lesson_Words[i] = buff.toString();
-            }
-        }
-        c.close();*/
-
-        txtword.setText(UpperCase[all_ctr] + P_Lesson_Words[all_ctr]);
+        //Database here
+        getData();
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(all_ctr < 25) {
+                if(all_ctr < (alphabet.length - 1)) {
                     if (mic_ctr == 0) {
                         txtresult.setText("Try it first!");
                     } else {
                         ++all_ctr;
                         mic_ctr = 0;
-                        txtword.setText(UpperCase[all_ctr] + P_Lesson_Words[all_ctr]);
+                        txtword.setText(alphabet[all_ctr]+alphabet[all_ctr].toLowerCase());
                         txtresult.setText("Press the Mic Button");
                         score += add;
                         add = 0;
 
-                        CurrentProgress = CurrentProgress +4;
+                        CurrentProgress = CurrentProgress + 370;
                         progressBar.setProgress(CurrentProgress);
-                        progressBar.setMax(100);
+                        progressBar.setMax(10000);
                     }
                 } else {
                     if (mic_ctr == 0) {
@@ -127,7 +137,7 @@ public class Alphabet extends AppCompatActivity {
             public void onClick(View v) {
                 stopPlaying();
                 Resources res = getResources();
-                int sound = res.getIdentifier(P_Lesson_Words[all_ctr], "raw", getPackageName());
+                int sound = res.getIdentifier(alphabet[all_ctr].toLowerCase(), "raw", getPackageName());
                 ai = MediaPlayer.create(Alphabet.this, sound);
                 ai.start();
             }
@@ -181,10 +191,9 @@ public class Alphabet extends AppCompatActivity {
 
             @Override
             public void onResults(Bundle bundle) {
-                //micButton.setImageResource(R.drawable.ic_mic_black_off);
                 ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
                 word = data.get(0);
-                printSimilarity(word.toString(),P_Lesson_Words[all_ctr]);
+                printSimilarity(word.toString(),alphabet[all_ctr]);
             }
 
             @Override
@@ -315,6 +324,77 @@ public class Alphabet extends AppCompatActivity {
             ai = MediaPlayer.create(Alphabet.this, R.raw.response_70_to_100);
             ai.start();
         }
+    }
+
+    private void getData() {
+
+        progressDialog.setTitle("Please wait");
+        progressDialog.setMessage("Loading lesson...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        String url = "https://biskwitteamdelete.000webhostapp.com/fetch_alphabet.php";
+
+        StringRequest stringRequest = new StringRequest(url, new com.android.volley.Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                showJSONS(response);
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Alphabet.this, error.getMessage().toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
 
     }
+
+    private void showJSONS(String response) {
+        ArrayList<String> letter = new ArrayList<String>();
+
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray result = jsonObject.getJSONArray(Constants.JSON_ARRAY);
+            int length = result.length();
+            for(int i = 0; i < length; i++) {
+                JSONObject collegeData = result.getJSONObject(i);
+                letter.add(collegeData.getString("letter"));
+            }
+            alphabet = new String[letter.size()];
+            alphabet = letter.toArray(alphabet);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        if(!alphabet[0].equals("")){
+            txtword.setText(alphabet[0]+alphabet[0].toLowerCase());
+            progressDialog.dismiss();
+        } else {
+            Toast.makeText(Alphabet.this, "No data", Toast.LENGTH_LONG).show();
+            progressDialog.dismiss();
+        }
+    }
+
+    // code para di magkeep playing yung sounds
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Exit now?")
+                .setMessage("You will not be able to save your progress.")
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Alphabet.super.onBackPressed();
+                        stopPlaying();
+                    }
+                }).create().show();
+    }
+
 }
--- app/src/main/java/com/example/biskwit/SignUp_Form.java
+++ app/src/main/java/com/example/biskwit/SignUp_Form.java
@@ -1,39 +1,28 @@
 package com.example.biskwit;
 
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
 import android.content.Intent;
 import android.view.View;
-import android.database.Cursor;
 import android.widget.Toast;
-
-import com.example.biskwit.Data.NetworkClient;
-import com.example.biskwit.Data.NetworkService;
-import com.example.biskwit.Data.RegistrationResponseModel;
-
+import com.example.biskwit.Data.Database;
 import java.util.HashMap;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SignUp_Form extends AppCompatActivity {
     Button SignUp,Login;
     public EditText Email, Password, Confirm_Password, Parents_Instructor, Name_Child, Age, Birthdate, Severity_Level;
     Intent intent;
-    DBHelper DB;
+    private static final String REGISTER_URL = "https://biskwitteamdelete.000webhostapp.com/insertdata.php";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_form);
-
-        // eto yung sa sign up forms natin na buttons
-        //di pa nakaset yung igeget niya yung input details ng user
 
         Email = findViewById(R.id.email);
         Password = findViewById(R.id.password);
@@ -44,34 +33,32 @@ public class SignUp_Form extends AppCompatActivity {
         Birthdate = findViewById(R.id.birthday);
         Severity_Level = findViewById(R.id.level_severity);
 
-        //DB = new DBHelper(this);
-
         SignUp = findViewById(R.id.signup);
         SignUp.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
 
-                if (Name_Child.getText().toString().equals("")) {
-                    Toast.makeText(SignUp_Form.this, "Enter first name", Toast.LENGTH_SHORT).show();
-                } else if (Email.getText().toString().equals("")) {
-                    Toast.makeText(SignUp_Form.this, "Enter email", Toast.LENGTH_SHORT).show();
+                if (Email.getText().toString().equals("")) {
+                    Toast.makeText(SignUp_Form.this, "Enter Email Address", Toast.LENGTH_SHORT).show();
+                } else if (!emailValidator(Email.getText().toString())) {
+                    Toast.makeText(SignUp_Form.this, "Enter Valid Email", Toast.LENGTH_SHORT).show();
                 } else if (Password.getText().toString().equals("")) {
-                    Toast.makeText(SignUp_Form.this, "Enter password", Toast.LENGTH_SHORT).show();
-                }/* else if (!emailValidator(inputemail.getText().toString())) {
-                    Toast.makeText(RegisterActivity.this, "Enter valid email", Toast.LENGTH_SHORT).show();
-
-                } */ else {
-
-                    HashMap<String, String> params = new HashMap<>();
-                    params.put("email", Email.getText().toString());
-                    params.put("password", Password.getText().toString());
-                    params.put("parent", Parents_Instructor.getText().toString());
-                    params.put("child", Name_Child.getText().toString());
-                    params.put("age", Age.getText().toString());
-                    params.put("birthday", Birthdate.getText().toString());
-                    params.put("severity", Severity_Level.getText().toString());
-                    register(params);
+                    Toast.makeText(SignUp_Form.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                } else if (!Password.getText().toString().equals(Confirm_Password.getText().toString())) {
+                    Toast.makeText(SignUp_Form.this, "Password Does not Match", Toast.LENGTH_SHORT).show();
+                }  else if (Parents_Instructor.getText().toString().equals("")) {
+                    Toast.makeText(SignUp_Form.this, "Enter Parent/Instructor Name", Toast.LENGTH_SHORT).show();
+                } else if (Name_Child.getText().toString().equals("")) {
+                    Toast.makeText(SignUp_Form.this, "Enter Child Name", Toast.LENGTH_SHORT).show();
+                } else if (Age.getText().toString().equals("")) {
+                    Toast.makeText(SignUp_Form.this, "Enter Child Age", Toast.LENGTH_SHORT).show();
+                } else if (Birthdate.getText().toString().equals("")) {
+                    Toast.makeText(SignUp_Form.this, "Enter Birthday", Toast.LENGTH_SHORT).show();
+                } else if (Severity_Level.getText().toString().equals("")) {
+                    Toast.makeText(SignUp_Form.this, "Enter Severity Level", Toast.LENGTH_SHORT).show();
+                } else {
+                    registerUser();
                 }
 
             }
@@ -80,49 +67,74 @@ public class SignUp_Form extends AppCompatActivity {
 
     }
 
-    private void register(HashMap<String, String> params) {
+    private void registerUser() {
 
-        final ProgressDialog progressDialog = new ProgressDialog(SignUp_Form.this);
-        progressDialog.setTitle("Please wait");
-        progressDialog.setMessage("Creating your account...");
-        progressDialog.setCancelable(false);
-        progressDialog.show();
+        String email = Email.getText().toString();
+        String password = Password.getText().toString();
+        String parent = Parents_Instructor.getText().toString();
+        String child = Name_Child.getText().toString();
+        String age = Age.getText().toString();
+        String birthday = Birthdate.getText().toString();
+        String severity = Severity_Level.getText().toString();
 
-        NetworkService networkService = NetworkClient.getClient().create(NetworkService.class);
-        Call<RegistrationResponseModel> registerCall = networkService.register(params);
-        registerCall.enqueue(new Callback<RegistrationResponseModel>() {
+        register(email,password,parent,child,age,birthday,severity);
+    }
+
+    private void register(String email,String password,String parent,String child,String age,String birthday,String severity) {
+        class RegisterUsers extends AsyncTask<String, Void, String> {
+            ProgressDialog loading;
+            Database ruc = new Database();
+
+
             @Override
-            public void onResponse(@NonNull Call<RegistrationResponseModel> call, @NonNull Response<RegistrationResponseModel> response) {
-                RegistrationResponseModel responseBody = response.body();
-                if (responseBody != null) {
-                    if (responseBody.getSuccess().equals("1")) {
-                        Toast.makeText(SignUp_Form.this, responseBody.getMessage(), Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(SignUp_Form.this, MainActivity.class);
-                        startActivity(intent);
-                        finish();
-                    } else {
-                        Toast.makeText(SignUp_Form.this, responseBody.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                }
-                progressDialog.dismiss();
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(SignUp_Form.this, "Creating your account...", null, true, true);
             }
 
             @Override
-            public void onFailure(@NonNull Call<RegistrationResponseModel> call, @NonNull Throwable t) {
-                progressDialog.dismiss();
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                    Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+                    intent = new Intent(SignUp_Form.this, MainActivity.class);
+                    startActivity(intent);
             }
-        });
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                HashMap<String, String> data = new HashMap<String, String>();
+
+                data.put("email", params[0]);
+                data.put("password", params[1]);
+                data.put("parent", params[2]);
+                data.put("child", params[3]);
+                data.put("age", params[4]);
+                data.put("birthday", params[5]);
+                data.put("severity", params[6]);
+
+                String result = ruc.sendPostRequest(REGISTER_URL, data);
+
+                return result;
+            }
+        }
+
+        RegisterUsers ru = new RegisterUsers();
+        ru.execute(email,password,parent,child,age,birthday,severity);
     }
 
-    //maglalabas ng message ito kapag pinindot nya ung signup button
-    public void Created_Account_Message()
-    {
-        //ung DialogueMessage class to wag idelete
-        DialogueMessage exampleDialog = new DialogueMessage();
-        exampleDialog.show(getSupportFragmentManager(),"example dialog");
+    public boolean emailValidator(String email) {
+        Pattern pattern;
+        Matcher matcher;
+        final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+
     }
-    //babalik ito sa login screen
-    public void Go_Login()
+
+    public void Go_Login(View v)
     {
         intent = new Intent(SignUp_Form.this, MainActivity.class);
         startActivity(intent);
