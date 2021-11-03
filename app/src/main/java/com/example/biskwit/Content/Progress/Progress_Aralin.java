9cddfe09e1e633a542d7a481c27b1f726d617128
chasenggg <80025784+chasenggg@users.noreply.github.com>
2021-11-03 16:28:24 +0800

    adjust

---
+++ app/src/main/java/com/example/biskwit/Content/Progress/Progress_Aralin.java
@@ -0,0 +1,17 @@
+package com.example.biskwit.Content.Progress;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.biskwit.R;
+
+public class Progress_Aralin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress_aralin);
+
+    }
+}
