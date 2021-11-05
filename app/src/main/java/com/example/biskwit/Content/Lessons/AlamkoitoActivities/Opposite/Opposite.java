0b100b04686818b5ab94b3df65dd0eb9ae55f6af
AshenAwakener <80026265+AshenAwakener@users.noreply.github.com>
2021-11-05 23:57:19 +0800

    adjust

--- app/src/main/java/com/example/biskwit/Content/Lessons/AlamkoitoActivities/Opposite/Opposite.java
+++ app/src/main/java/com/example/biskwit/Content/Lessons/AlamkoitoActivities/Opposite/Opposite.java
@@ -1,19 +1,35 @@
 package com.example.biskwit.Content.Lessons.AlamkoitoActivities.Opposite;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.biskwit.Content.Lessons.OrtonActivities.Phoenimic;
+import com.example.biskwit.Data.Constants;
 import com.example.biskwit.R;
-
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class Opposite extends AppCompatActivity {
@@ -22,13 +38,17 @@ public class Opposite extends AppCompatActivity {
     ImageButton mic1,mic2;
     ImageView next,bot,bot2;
     String word;
-    String[] words1 = {"masaya","maganda","masarap","mabagal","maingay","mayaman","malungkot","mababa","mataas","mabango"};
-    String[] words2 = {"maligaya","marikit","malinamnam","makupad","magulo","masalapi","malumbay","pandak","matangkad","mahalimuyak"};
+    String[] words1;
+    String[] words2;
+    String[] holder;
     int all_ctr = 0, click = 0, micctr1 = 0, micctr2 = 0;
+    double score = 0, add = 0;
     MediaPlayer ai;
 
     public static final Integer RecordAudioRequestCode = 1;
     private SpeechRecognizer speechRecognizer;
+
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,8 +63,7 @@ public class Opposite extends AppCompatActivity {
         bot2 = findViewById(R.id.Bot2);
         next = findViewById(R.id.nextButton);
 
-        word1.setText(words1[all_ctr]);
-        word2.setText(words2[all_ctr]);
+        getData();
 
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
 
@@ -85,7 +104,6 @@ public class Opposite extends AppCompatActivity {
 
             @Override
             public void onResults(Bundle bundle) {
-                //micButton.setImageResource(R.drawable.ic_mic_black_off);
                 if(micctr1>0) {
                     ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
                     word = data.get(0);
@@ -110,8 +128,6 @@ public class Opposite extends AppCompatActivity {
 
             }
         });
-
-
 
         mic1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -147,15 +163,25 @@ public class Opposite extends AppCompatActivity {
             }
         });
 
-
-        bot.setOnClickListener(new View.OnClickListener() {
+        word1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 stopPlaying();
-                //Resources res = getResources();
-                //int sound = res.getIdentifier(P_Lesson_Words[all_ctr], "raw", getPackageName());
-                //ai = MediaPlayer.create(Alphabet.this, sound);
-                //ai.start();
+                Resources res = getResources();
+                int sound = res.getIdentifier(words1[all_ctr].toLowerCase(), "raw", getPackageName());
+                ai = MediaPlayer.create(Opposite.this, sound);
+                ai.start();
+            }
+        });
+
+        word2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopPlaying();
+                Resources res = getResources();
+                int sound = res.getIdentifier(words2[all_ctr].toLowerCase(), "raw", getPackageName());
+                ai = MediaPlayer.create(Opposite.this, sound);
+                ai.start();
             }
         });
 
@@ -176,16 +202,19 @@ public class Opposite extends AppCompatActivity {
 
                 stopPlaying();
 
-                //pampagrayscale lang to nung bot na icon
-                //ColorMatrix matrix = new ColorMatrix();
-                //matrix.setSaturation(0);
-                //bot.setColorFilter(new ColorMatrixColorFilter(matrix));
             }
         });
     }
 
-    public void toastMsg(String msg) {
-        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+    public void showToast(String s) {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast, (ViewGroup) findViewById(R.id.toast_root));
+        TextView toastText = layout.findViewById(R.id.toast_text);
+        toastText.setText(s);
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
         toast.show();
     }
 
@@ -242,17 +271,105 @@ public class Opposite extends AppCompatActivity {
         float val = Float.parseFloat(String.format(
                 "%.3f", similarity(s, t), s, t));
         if(val >= 0.0 && val <= 0.49){
+            add = 0;
+            showToast("TRY AGAIN");
             ai = MediaPlayer.create(Opposite.this, R.raw.response_0_to_49);
             ai.start();
         }
         else if(val >= 0.5 && val <= 0.99){
+            add = 0.5;
+            showToast("GOOD, BUT YOU CAN DO BETTER");
             ai = MediaPlayer.create(Opposite.this, R.raw.response_50_to_69);
             ai.start();
         }
         else if(val ==1.0){
+            add = 1;
+            showToast("GREAT! YOU DID IT!");
             ai = MediaPlayer.create(Opposite.this, R.raw.response_70_to_100);
             ai.start();
         }
+    }
 
+    private void getData() {
+        progressDialog.setTitle("Please wait");
+        progressDialog.setMessage("Loading lesson...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        String url = "https://biskwitteamdelete.000webhostapp.com/fetch_opposite.php";
+
+        StringRequest stringRequest = new StringRequest(url, new com.android.volley.Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                showJSONS(response);
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Opposite.this, error.getMessage().toString(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+    }
+
+    private void showJSONS(String response) {
+        ArrayList<String> data2 = new ArrayList<String>();
+
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray result = jsonObject.getJSONArray(Constants.JSON_ARRAY);
+            int length = result.length();
+            for(int i = 0; i < length; i++) {
+                JSONObject collegeData = result.getJSONObject(i);
+                data2.add(collegeData.getString("word"));
+            }
+            holder = new String[data2.size()];
+            holder = data2.toArray(holder);
+
+            int holder_ctr=0;
+            words1 = new String[holder.length/10];
+            words2 = new String[holder.length/10];
+            for (int i = 0; i < 10; i++) {
+                words1[i] = holder[holder_ctr];
+                holder_ctr++;
+            }
+            for (int k = 0; k < 10; k++) {
+                words2[k] = holder[holder_ctr];
+                holder_ctr++;
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        if(!holder[0].equals("")){
+            word1.setText(words1[0]);
+            word2.setText(words2[0]);
+            progressDialog.dismiss();
+        } else {
+            Toast.makeText(Opposite.this, "No data", Toast.LENGTH_LONG).show();
+            progressDialog.dismiss();
+        }
+    }
+
+    // code para di magkeep playing yung sounds
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setTitle("Exit now?")
+                .setMessage("You will not be able to save your progress.")
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Opposite.super.onBackPressed();
+                        stopPlaying();
+                    }
+                }).create().show();
     }
 }
