1dc34187ddb8f3f3267512f86baf093491884818
chasenggg <80025784+chasenggg@users.noreply.github.com>
2021-11-19 13:51:28 +0800

    final design adjustment(10)

--- app/src/main/java/com/example/biskwit/MainActivity/Contact_Info.java
+++ app/src/main/java/com/example/biskwit/MainActivity/Contact_Info.java
@@ -2,17 +2,18 @@ package com.example.biskwit.MainActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.biskwit.R;
 
 public class Contact_Info extends AppCompatActivity {
-    Button backbutton;
+    ImageView backbutton;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contactinfo);
-        backbutton = (Button) findViewById(R.id.back);
+        backbutton = (ImageView) findViewById(R.id.back);
         backbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
