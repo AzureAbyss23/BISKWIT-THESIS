71088c3336036a1669808e6071bb42b54a40baaa
chasenggg <80025784+chasenggg@users.noreply.github.com>
2021-11-24 12:22:42 +0800

    final design adjustment(13)

--- app/src/main/java/com/example/biskwit/Content/EasyFragment.java
+++ app/src/main/java/com/example/biskwit/Content/EasyFragment.java
@@ -35,7 +35,7 @@ public class EasyFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         // eto yung code para sa Aralin para sa patinig na button natin
-        binding.Alphabet.setOnClickListener(new View.OnClickListener() {
+        binding.Abakada.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Fragment fragmentAlphabet = new AbakadaFragment();
